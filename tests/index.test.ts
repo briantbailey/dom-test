7a9ff40d1e56421eae2ac6365be258496027f62e
Brian T Bailey <briantbailey@gmail.com>
2025-09-28 10:52:51 -0500

    fix test import

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from 'vitest'
-import { myFunction } from '../src'
+import { expect, test } from "vitest";
+import { myFunction } from "../src/dom-test";
 
-test('myFunction', () => {
-  expect(myFunction()).toBe('Hello, world!')
-})
+test("myFunction", () => {
+  expect(myFunction()).toBe("Hello, world!");
+});
