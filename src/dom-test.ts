fc05104197d285953b4ec7f0169ddcb45fe82ea4
Brian T Bailey <briantbailey@gmail.com>
2025-09-28 11:30:16 -0500

    setup jsr

--- src/dom-test.ts
+++ src/dom-test.ts
@@ -1,11 +1,11 @@
-export const myFunction = () => {
+export const myFunction = (): string => {
   return "Hello, world!";
 };
 
-export const helloString = (string: string) => {
+export const helloString = (string: string): string => {
   return `Hello, ${string}!`;
 };
 
-export const doubleString = (string: string) => {
+export const doubleString = (string: string): string => {
   return string + " " + string;
 };
